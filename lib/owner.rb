d53fd02d1692ae31841f412f204bfd9546539852
Joe Ganobsik <jganobsik@gmail.com>
2018-12-27 13:55:07 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,27 @@
 class Owner
-  # code goes here
+  attr_accessor :pets, :name 
+  attr_reader :species
+  @@all_owners = []
+  
+  def initialize(name)
+    @name =  name
+    @pets = {:cats=>[], :dogs=>[], :fishes=>[]}
+    @species = 'human'
+    @@all_owners << self
+  end
+  
+  def say_species
+    return "I am a #{@species}."
+  end
+  
+  def self.reset_all 
+    @@all_owners.clear
+  end
+  
+  def self.all 
+    @@all_owners  
+  end
+  def self.count
+    @@all_owners.length
+  end
 end
